bd4dbd71dce9565a3cb04f07f610e95a7837063b
Aswin Thomas <aswin@nutonomy.com>
2015-07-31 16:17:24 +0800

    adding cmake modules install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -56,4 +56,6 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 sudo rosdep init | sed "s/ERROR: default/WARNING: default/"
 rosdep update
 
+# Other packages
+sudo apt-get install -y ros-hydro-cmake-modules
 set -e
