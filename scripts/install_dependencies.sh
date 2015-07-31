9256a413236b6dd81bb718d0ba71968fb0b20c76
Aswin Thomas <aswin@nutonomy.com>
2015-07-31 15:49:53 +0800

    moving to hydro

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -17,28 +17,39 @@ sourceROS
 
 set +e
 # Check Ubuntu release
-codename=`lsb_release --codename | cut -f2`
-echo "Ubuntu "$codename
-case $codename in
-  # | --- indigo --- |   
-  # | 13.10 |  14.04 |
-  "saucy"|"trusty");;
-  *) 	echo "Ubuntu `lsb_release --release | cut -f2` not supported. Manual installation is suggested."
-  if confirm; then
-    exit
-  fi;;
-esac
+#codename=`lsb_release --codename | cut -f2`
+#echo "Ubuntu "$codename
+#case $codename in
+#  # | --- indigo --- |   
+#  # | 13.10 |  14.04 |
+#  "saucy"|"trusty");;
+#  *) 	echo "Ubuntu `lsb_release --release | cut -f2` not supported. Manual installation is suggested."
+#  if confirm; then
+#    exit
+#  fi;;
+#esac
 
-# Install ROS
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu" `lsb_release --codename | cut -f2` "main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+# Install ROS hydro
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list' 
+wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
 sudo apt-get update
-if [ ! -z "$(apt-cache search ros-indigo-desktop-full)" ]; then
-  export ROS_DISTRO=indigo
+if [ ! -z "$(apt-cache search ros-hydro-desktop-full)" ]; then
+  export ROS_DISTRO=hydro
 else
-  echo "ROS indigo cannot be automatically installed on the current OS. Please proceed manually."
+  echo "ROS hydro cannot be automatically installed on the current OS. Please proceed manually."
   exit
 fi
+
+# Install ROS indigo
+#sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu" `lsb_release --codename | cut -f2` "main" > /etc/apt/sources.list.d/ros-latest.list'
+#sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+#sudo apt-get update
+#if [ ! -z "$(apt-cache search ros-indigo-desktop-full)" ]; then
+#  export ROS_DISTRO=indigo
+#else
+#  echo "ROS indigo cannot be automatically installed on the current OS. Please proceed manually."
+#  exit
+#fi
 
 sudo apt-get install -y ros-$ROS_DISTRO-desktop-full
 source /opt/ros/$ROS_DISTRO/setup.bash
